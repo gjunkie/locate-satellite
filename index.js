ab4247c79147aa7417f978dadc751cdc0873935b
Freddie Carthy <fcarthy@gmail.com>
2014-09-30 22:16:27 -0700

    location stream, tests. WIP: failing tests

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 var request = require('request')
-var Stream = require('stream').Readable;
+var Stream = require('stream');
 var util = require('util');
 
-module.exports = Satellite;
+exports.Satellite = Satellite;
+exports.LocationStream = LocationStream;
 
 /**
  * Set up module
@@ -16,15 +17,17 @@ function Satellite(id, options) {
   this.options = options || {};
   this.options.objectMode = true;
 
-  Stream.call(this, this.options);
+  Stream.Readable.call(this, this.options);
 
   var self = this;
 
+  this.running = false;
   this.id = id || 25544;
   this.options.api = this.options.api || 'https://api.wheretheiss.at/v1/satellites/' + this.id;
   this.options.rate = options.rate || 1000;
   this.options.objectMode = true;
 
+  // prevent back-to-back requests faster than 1 second
   if (options.rate < 1000) {
     return new Error('rate must be at least 1000 milliseconds');
   }
@@ -34,31 +37,39 @@ function Satellite(id, options) {
   return this;
 }
 
-util.inherits(Satellite, Stream);
+util.inherits(Satellite, Stream.Readable);
 
 /**
  * Fetch data from specified api
  */
 Satellite.prototype._fetchData = function() {
   var self = this;
-  clearTimeout(this.timer);
 
-  request({uri: self.options.api, strictSSL: false}, function(error, response, data) {
-    if (response.statusCode !== 200) {
-      self.stop();
-      return new Error(data.error);
-    }
-    var result = JSON.parse(data);
-    self.push(result);
-    self._finished();
-  });
+  // make sure we only run 1 request at a time
+  if (!this.running) {
+    request({uri: self.options.api, strictSSL: false}, function(error, response, data) {
+      if (response.statusCode !== 200) {
+        self.stop();
+        return new Error(data.error);
+      }
+      var result = JSON.parse(data);
+      self.push(result);
+      self._finished();
+    });
+  }
 }
 
 /**
  * Finished streaming data, set the timer 
  */
 Satellite.prototype._finished = function() {
-  this.timer = setTimeout(this._fetchData(), this.options.rate);
+  this.running = false;
+
+  var self = this;
+  this.timer = setTimeout(function(){
+    self.running = true;
+    self._fetchData();
+  }, this.options.rate);
 }
 
 /**
@@ -75,3 +86,38 @@ Satellite.prototype.stop = function () {
   clearTimeout(this.timer);
   this.push(null);
 }
+
+
+/**
+ * Stream the change in location
+ */
+function LocationStream() {
+  Stream.Transform.call(this, { objectMode: true });
+
+  return this;
+}
+util.inherits(LocationStream, Stream.Transform);
+
+/**
+ * Clear the timeout and finish streaming
+ *
+ * @param chunk: obj, satellite location oject
+ * @param encoding: string, encoding type, defaults to utf8
+ * @param callback: function, callback function
+ */
+LocationStream.prototype._transform = function(chunk, encoding, callback){
+  if (this.oldResult) {
+    var timeDelta = (chunk.timestamp - this.oldResult.timestamp)/1000;
+    var delta = {
+      latitude_delta: (chunk.latitude - this.oldResult.latitude) / timeDelta,
+      longitude_delta: (chunk.longitude - this.oldResult.longitude) / timeDelta
+    }
+
+    if (delta.latitude_delta !== 0 && delta.longitude_delta !== 0) {
+      this.push(delta);
+    }
+  }
+  this.oldTime = new Date();
+  this.oldResult = chunk;
+  callback();
+};
--- test/index.js
+++ test/index.js
@@ -5,10 +5,12 @@ var Satellite = require('../');
 describe('satellite', function() {
 
   it('should get data from api', function(done) {
-    var satellite = new Satellite(25544, { rate: 1000 });
+    var satellite = new Satellite.Satellite(25544, { rate: 1000 });
     satellite.pipe(through.obj(function(chunk, enc, callback) {
       if (chunk) {
         chunk.should.be.a('object');
+        //console.log(chunk);
+        //var location = new Satellite.LocationStream(chunk);
         this.emit('close');
         done();
       }
@@ -17,7 +19,7 @@ describe('satellite', function() {
   });
 
   it('data should be from satellite requested', function(done) {
-    var satellite = new Satellite(25544, { rate: 1000 });
+    var satellite = new Satellite.Satellite(25544, { rate: 1000 });
     var id = 25544;
     satellite.pipe(through.obj(function(chunk, enc, callback) {
       if (chunk) {
@@ -30,7 +32,7 @@ describe('satellite', function() {
   });
 
   it('data should have latitude and longitude', function(done) {
-    var satellite = new Satellite(25544, { rate: 1000 });
+    var satellite = new Satellite.Satellite(25544, { rate: 1000 });
     satellite.pipe(through.obj(function(chunk, enc, callback) {
       if (chunk) {
         chunk.latitude.should.be.a('number');
@@ -43,9 +45,44 @@ describe('satellite', function() {
   });
 
   it('fetch rate should be at least 1000 milliseconds', function(done) {
-    var satellite = new Satellite(25544, { rate: 500 });
+    var satellite = new Satellite.Satellite(25544, { rate: 500 });
     satellite.should.be.an.instanceOf(Error);
     done();
   });
+
+});
+
+describe('location', function() {
+
+  it('should stream location data', function(done) {
+    this.timeout(3000);
+    var location = new Satellite.LocationStream();
+    var satellite = new Satellite.Satellite(25544, { rate: 1000 });
+    satellite.pipe(location).pipe(through.obj(function(chunk, enc, callback) {
+      if (chunk) {
+        chunk.latitude_delta.should.be.a('number');
+        chunk.longitude_delta.should.be.a('number');
+        this.emit('close');
+        done();
+      }
+      callback();
+    }));
+  });
+
+  it('should have delta on every request', function(done) {
+    this.timeout(3000);
+    var location = new Satellite.LocationStream();
+    var satellite = new Satellite.Satellite(25544, { rate: 1000 });
+    satellite.pipe(location).pipe(through.obj(function(chunk, enc, callback) {
+      if (chunk) {
+        chunk.latitude_delta.should.not.equal(0);
+        chunk.longitude_delta.should.not.equal(0);
+        this.emit('close');
+        done();
+      }
+      callback();
+    }));
+  });
+
 
 });
